652d8c3f4017fa317c5fc527e2d9585e791797d1
danielmirchandani <danielmirchandani@users.noreply.github.com>
2024-06-23 23:42:15 -0400

    Very basic scripts I execute from other scripts.

---
+++ src/hack.ts
@@ -0,0 +1,15 @@
+import type {NS} from '../NetscriptDefinitions.d.ts';
+
+export async function main(ns: NS): Promise<void> {
+  const flags = ns.flags([
+    ['delay', -1],
+    ['target', ''],
+  ]);
+  if (typeof flags.delay !== 'number') {
+    throw new Error(`--delay must be a number, got ${flags.delay}`);
+  }
+  if (typeof flags.target !== 'string') {
+    throw new Error(`--target must be a string, got ${flags.target}`);
+  }
+  await ns.hack(flags.target, {additionalMsec: flags.delay});
+}
